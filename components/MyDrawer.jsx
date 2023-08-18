4a871564dd9d1fa12a1face86016679c77def0ab
r0j05cr <rishav.jaiswal@walmart.com>
2023-08-18 19:42:26 +0530

    form changes

--- components/MyDrawer.jsx
+++ components/MyDrawer.jsx
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from "./Home";
 import Notifications from "./Notifications";
 import { SafeAreaView } from "react-native-safe-area-context";
+import FormExample from "./ReactFormExample";
 
 const Drawer = createDrawerNavigator();
 const MyDrawer = () => {
@@ -18,6 +19,7 @@ const MyDrawer = () => {
                     initialRouteName="Home">
                     <Drawer.Screen name="Home" component={HomeScreen} />
                     <Drawer.Screen name="Notifications" component={Notifications} />
+                    <Drawer.Screen name="FormExample" component={FormExample} />
                 </Drawer.Navigator>
             </NavigationContainer>
         </>
